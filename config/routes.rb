77952931681808350def83c1d0deb1c58e7f7665
Stanley Surjanto <stan.surjanto@gmail.com>
2022-10-18 17:28:59 -0600

    uses the article_path(any article) as a helper in the index views

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-
   # Mapping our main/root page to the index action in articles controller
   root "articles#index"
 
-
-
-
-
-  # all GET to '/articles' mapped to the 'index' class method/action of ArticlesController
-  get "/articles", to: "articles#index"
-  # to create 'ArticlesController' with index action, run controller generator following the 6.1 rails guide
-
-  # Next route, we want to view the article in full, title AND body
-  get "/articles/:id", to: "articles#show" 
-  # similarly, hand off to articles controller, but SHOW action this time
-  # :id = params[:id] (similar to Express and Node!!)
-
+  # This simple command generates all most common routes for a resource type
+  # seriously, it creates GETs, PUTs, POSTs, DELETEs IMMEDIATELY
+  # To inspect which routes were made console-> bin/rails routes 
+  resources :articles
+  # The "Prefix" column + suffix of _url or _path form the names of path helpers 
+  # For example, `article_path` helper returns "/articles/#{article.id}" when given an article.
 end
 
 # Remember this mantra...
