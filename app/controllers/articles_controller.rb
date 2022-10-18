68cdb55b7adfde81e52bc4ecadf1b3cb996e2e87
Stanley Surjanto <stan.surjanto@gmail.com>
2022-10-18 16:48:54 -0600

    added a <ul> <li> of article titles to the index html views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,16 @@
+# created during ArticlesController generator command (follow rails 6.1.7 tutorial)
 class ArticlesController < ApplicationController
   def index
+    # to fetch all articles from the database
+    @articles = Article.all
+    # Controller INSTANCE variables, can be accessed by the view
+    # Meaning... we can reference @articles in app/views/articles/index.html.erb
   end
 end
+
+# Anything inside the index method here is delivered to the views html which the browser sees, served by our server (similarly to EJS)
+
+# <% %> and <%= %> inside the HTML holds ERB (embedded ruby) code
+
+# <% %> : Run the ERB here WITHOUT showing to views
+# <%= %> : Run the ERB here AND show to views
