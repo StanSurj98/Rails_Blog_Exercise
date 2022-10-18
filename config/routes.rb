17e8c09a0a117d0a1a15ba9d638d826370fb3125
Stanley Surjanto <stan.surjanto@gmail.com>
2022-10-18 15:55:18 -0600

    from config/routes.rb, add route to /articles and generated the controller relevant files

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # all GET to '/articles' mapped to the 'index' class method/action of ArticlesController
+  get "/articles", to: "articles#index"
+  # to create 'ArticlesController' with index action, run controller generator following the 6.1 rails guide
 end
