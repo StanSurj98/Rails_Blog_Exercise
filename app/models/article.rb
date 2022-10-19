a50757a4b4e2e8bb03731d6b3ea6ceecccddea67
Stanley Surjanto <stan.surjanto@gmail.com>
2022-10-18 18:09:17 -0600

    add validations to the Article model, and also <divs> that hold our possible error messages in the /views.../new.html.erb

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,11 @@
   # we want the model to be singular so tables are "articles" in plural
 
 class Article < ApplicationRecord
+  # Add in validation for when users create new articles
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 10 }
 end
+
+# Note: 
+# ActiveRecord automatically defined :title, :body, :other_model_attributes
+# automatically for each table column, no need to define it ourselves
